7b6d54752cd8b8fb0f4c6fa468ef85bb46febf88
cereceres <jesus.edelcereceres@gmail.com>
2015-08-14 13:48:29 -0500

    No commit

--- test/test.js
+++ test/test.js
@@ -85,6 +85,12 @@ describe('JNsolve Module numeric values function test.', function () {
             });
             it('Matrix constructor instance a object', function () {
                   assert.equal( new JNsolve.AL.matrix([3,2]) instanceof JNsolve.AL.matrix,true ); // should returns true
+
+            });
+
+            it('Inverse method calculate the inverse a matrix', function () {
+                  assert.equal(  JNsolve.AL.inv(new JNsolve.AL.matrix([[3,2],[1,2]]) ).array instanceof Array,true ); // should returns true
+
             });
 });
 
