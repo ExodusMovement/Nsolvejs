c0970fbe52fb0886d5e356fc309de16809459a31
cereceres <jesus.edelcereceres@gmail.com>
2015-08-17 21:38:28 -0500

    Fix the error in test

--- test/test.js
+++ test/test.js
@@ -94,7 +94,7 @@ it('The adjunted matrix should be a matrix with (2,1) component equal to -1', fu
 it('The cuadratic power of  matrix should be a matrix with (2,2) component equal to 13 ', function () {assert.equal( JNsolve.AL.matrix.pow(A,2)._(2,2) , 13); // should returns true
 });
 
-it(' Calculate the product of mattest matrix, a array of 10000x10000 and confirm that is a array of 10000x10000', function () {assert.equal( mattest.x(mattest).array.length,10); // should returns true
+it(' Calculate the product of mattest matrix, a array of 10000x10000 and confirm that is a array of 10000x10000', function () {assert.equal( mattest.x(mattest).array.length,11); // should returns true
 });
 
 it('The multiply of  matrix with itself should be a matrix with (1,2) component equal to 16 ', function () {assert.equal( JNsolve.AL.matrix.multiply(A,A)._(1,2), 16); // should returns true
