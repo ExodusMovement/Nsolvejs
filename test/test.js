d1f4ca82d2a9c0df72a00032968d4c97d67fb2b7
cereceres <jesus.edelcereceres@gmail.com>
2015-12-10 20:53:28 -0600

    the stats module is now available

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 'use strict';
 var assert = require('assert'),
   JNsolve = require('../index'),
+  data,
+  stats,
   test_array = [
     [0, 40],
     [1, 48],
@@ -37,7 +39,7 @@ mat = require('./mat_test');
 var mattest = new JNsolve.AL.matrix(mat);
 describe('JNsolve Module numeric values function test.', function () {
   it('JNsolve should be a object', function () {
-    assert.equal(typeof JNsolve, 'object'); // should returns true
+    assert.equal(typeof JNsolve, 'function'); // should returns true
   });
 
   it('Found the correct solution to cos(x)-x=0 is 0.73 using the regulafalsi method.', function () {
@@ -103,31 +105,21 @@ describe('JNsolve Module numeric values function test.', function () {
     assert.equal(typeof fitted.fit.best.f, 'function'); // should returns true
   });
 
-<<<<<<< HEAD
   it('The adjunted matrix should be a matrix with (2,1) component equal to -1', function () {
     assert.equal(JNsolve.AL.matrix.adj(A)._(2, 1), -1); // should returns true
   });
-=======
-it('The adjunted matrix should be a matrix with (2,1) component equal to -1', function () {
-                  assert.equal( JNsolve.AL.matrix.adj(A)._(2,1) , -1); // should returns true
-            });
-it('The matrix method ones generates a matrix with only ones', function () {
-      assert.equal( JNsolve.AL.matrix.ones(6,8)._(6,8) , 1); // should returns true
-});
-it('The matrix method zeros generates a matrix with only zeros', function () {
-      assert.equal( JNsolve.AL.matrix.zeros(15,18)._(3,11) , 0); // should returns true
-});
-it('The product direct of A with itself has 1,2 component equal 1 ', function () {
-      assert.equal( A._x(A)._(1,2) , 16); // should returns true
-});
-it('The pow direct of A to seven power has 2,2 component equal 0 ', function () {
-      assert.equal( A._pow(7)._(2,2) , Math.pow(3,7)); // should returns true
-});
-
-
-
-
->>>>>>> 7b0be087aa3eb6b9a2db2ab7c6ff857d46cdc7e8
+  it('The matrix method ones generates a matrix with only ones', function () {
+    assert.equal(JNsolve.AL.matrix.ones(6, 8)._(6, 8), 1); // should returns true
+  });
+  it('The matrix method zeros generates a matrix with only zeros', function () {
+    assert.equal(JNsolve.AL.matrix.zeros(15, 18)._(3, 11), 0); // should returns true
+  });
+  it('The product direct of A with itself has 1,2 component equal 1 ', function () {
+    assert.equal(A._x(A)._(1, 2), 16); // should returns true
+  });
+  it('The pow direct of A to seven power has 2,2 component equal 0 ', function () {
+    assert.equal(A._pow(7)._(2, 2), Math.pow(3, 7)); // should returns true
+  });
 
   it('The cuadratic power of  matrix should be a matrix with (2,2) component equal to 13 ', function () {
     assert.equal(JNsolve.AL.matrix.pow(A, 2)._(2, 2), 13); // should returns true
@@ -261,4 +253,60 @@ describe('Negative cases.', function () {
     assert.equal(JNsolve.calculusN.findroot(undefined, interval, initialpoint), undefined); // should returns true
   });
 
+});
+
+describe('Stats module test', function () {
+  before(function() {
+    // create a instance of stats object
+    data = [
+      [3,4,5,2,1,5,6],
+      [1,4,0,4,1,5,6],
+      [6,4,5,2,1,5,1],
+      [3,4,5,5,0,5,4],
+      [4,4,5,2,1,5,12],
+      [0,4,0,9,1,5,3],
+      [6,4,3,2,0,5,6]
+    ]
+    stats = new JNsolve.Stats(data)
+  });
+
+
+  it('the data property is the array passed to class Stats ',
+  function () {
+    assert.equal(stats.data.length,data.length);
+    assert.equal(stats.data[0][0],data[0][0]); /// should returns true
+  });
+
+  it('the media of datas given is [ [ 3.2857142857142856 ],[ 4 ],[ 3.2857142857142856 ],[ 3.714285714285714 ],[ 0.7142857142857142 ],[ 5 ],[ 5.428571428571428 ] ] ',
+  function () {
+    assert.equal(stats.media()._(1,1),3.2857142857142856);
+    assert.equal(stats.media()._(2,1),4);
+    assert.equal(stats.media()._(3,1),3.2857142857142856);
+    assert.equal(stats.media()._(4,1),3.714285714285714);
+    assert.equal(stats.media()._(5,1),0.7142857142857142);
+    assert.equal(stats.media()._(6,1),5);
+    assert.equal(stats.media()._(7,1),5.428571428571428);
+   /// should returns true
+  });
+
+  it('the matrix correlation is given by:',
+  function () {
+  assert.equal(stats.std().diagonal()._(1,1),2.288688541085317);
+  assert.equal(stats.std().diagonal()._(2,1),0);
+  assert.equal(stats.std().diagonal()._(3,1),2.3603873774083293);
+  assert.equal(stats.std().diagonal()._(4,1),2.627691364061218);
+  assert.equal(stats.std().diagonal()._(5,1), 0.48795003647426666);
+  assert.equal(stats.std().diagonal()._(6,1),0);
+  assert.equal(stats.std().diagonal()._(7,1),3.4572215654165053);
+   /// should returns true
+  });
+  it('the matrix correlation is given by:[ [ 0.4369314487526515 ],[ NaN ],[ 0.42365927286816174 ],[ 0.38056219755369364 ],[ 2.0493901531919194 ],[ NaN ], [ 0.2892496130428152 ] ]',
+  function () {
+  assert.equal(stats.covariance().diagonal()._(1,1),0.4369314487526515);
+  assert.equal(stats.covariance().diagonal()._(3,1),0.42365927286816174);
+  assert.equal(stats.covariance().diagonal()._(4,1),0.38056219755369364);
+  assert.equal(stats.covariance().diagonal()._(5,1), 2.0493901531919194 );
+  assert.equal(stats.covariance().diagonal()._(7,1),0.2892496130428152);
+   /// should returns true
+  });
 });
